ed96b9900ad2f61770613de42689887af88ae813
Venu005 <venusaiyalamanchili@gmail.com>
2025-05-27 16:11:02 +0530

    feat: add authentication middleware to protect routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,17 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
-export default clerkMiddleware();
+const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)", "/"]);
+
+export default clerkMiddleware(async (auth, req) => {
+  if (!isPublicRoute(req)) {
+    const { userId } = await auth();
+    if (!userId) {
+      // redirect to sign-in page if user is not authenticated
+      return NextResponse.redirect(new URL("sign-in", req.url));
+    }
+  }
+});
 
 export const config = {
   matcher: [
